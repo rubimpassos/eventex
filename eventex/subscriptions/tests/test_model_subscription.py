f24cd2bef1868f7daffe157c4bb3499c2b27c397
Rubi Mendes Passos <rubimpassos@gmail.com>
2018-07-14 03:41:19 -0300

    Configuring django admin

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -7,7 +7,7 @@ from eventex.subscriptions.models import Subscription
 class SubscriptionModelTest(TestCase):
     def setUp(self):
         self.obj = Subscription(
-            name='Nome completo',
+            name='Nome Fictício',
             cpf='12345678901',
             email='nome@email.com.br',
             phone='21-987498740'
@@ -20,3 +20,6 @@ class SubscriptionModelTest(TestCase):
     def test_created_at(self):
         """Subscription must have an auto created_at attr"""
         self.assertIsInstance(self.obj.created_at, datetime)
+
+    def test_str(self):
+        self.assertEqual('Nome Fictício', str(self.obj))
